881b41b7cd19e09165842fdb1b0ea53a78a6a6cb
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-09-16 20:48:01 +0200

    act

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,24 +6,23 @@
 /*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 20:19:11 by omartine          #+#    #+#             */
-/*   Updated: 2021/09/16 20:21:46 by omartine         ###   ########.fr       */
+/*   Updated: 2021/09/16 20:47:22 by omartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdio.h>
-
-int ft_isalpha(char x)
+int	ft_isalpha(char x)
 {
-    if (x >= 'A' && x <= 'Z' || x >='a' && x <= 'z')
-        return 1;
-    return 0;
+	if (x >= 'A' && x <= 'Z' || x >='a' && x <= 'z')
+		return (1);
+	return (0);
 }
 
-int main(void)
+int	main(void)
 {
-    int i;
-    char x = 'x';
-    i = ft_isalpha(x);
-    return i;
+	int		i;
+	char	x;
+
+	x = 'x';
+	i = ft_isalpha(x);
+	return (i);
 }
