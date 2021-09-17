7133c971c3e17dfc3d2b5239b11f0366954e3dfb
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-09-17 21:19:10 +0200

    subida memset

---
+++ ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 20:43:49 by omartine          #+#    #+#             */
+/*   Updated: 2021/09/17 21:18:50 by omartine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memset(void *str, int c, int size)
+{
+	int				i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)str;
+	i = 0;
+	while (i != size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return ((void *)ptr);
+}
+
+int main(void)
+{
+	char str[] = "hola adios";
+	ft_memset(str, '$', 4);
+	printf("%s", str);
+	return (0);
+}
