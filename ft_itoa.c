4dbb337eb23928fbf23cebc53c52ea5a50d3cec3
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-10-01 21:35:23 +0200

    viernes 1

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,60 +6,51 @@
 /*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 15:59:47 by omartine          #+#    #+#             */
-/*   Updated: 2021/09/30 21:09:42 by omartine         ###   ########.fr       */
+/*   Updated: 2021/10/01 21:34:14 by omartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_aux(int n, int j, char *aux, int sb)
+static void	ft_aux(long n, int j, char *aux)
 {
-	if (n < 10)
+	if (n == 0)
+		aux[j - 1] = '0';
+	while (n != 0)
 	{
-		j--;
-		aux[j] = '0' + n;
-		//printf("(+%c+-%d-)", aux[j], j);
-	}
-	else
-	{
-		ft_aux(n / 10, (j-- - 1), aux, sb);
-		aux[j] = '0' + (n % 10);
-		//printf("(+%c+-%d-)", aux[j], j);
+		aux[j-- - 1] = (n % 10) + '0';
+		n = n / 10;
+		//printf("%c", aux[j + 1]);
 	}
 }
 
 char	*ft_itoa(int n)
 {
 	char	*aux;
-	int		i;
+	long	i;
 	int		j;
-	int		sb;
+	long	sb;
 
-	j = 0;
+	j = 1;
 	i = 1;
-	if (n < 0)
-	{
-		j++;
-		sb = -1;
-		n = sb * n;
-	}
-	while (i < n)
-	{
-		i *= 10;
-		j++;
-	}
-	aux = (char *) malloc (sizeof(char) * j);
+	sb = n;
+	if (n <= 0 && j++ != -1)
+		sb = sb * -1;
+	while (i <= sb && j++ != -1)
+		i = 10 * i;
+	aux = (char *) malloc (sizeof(char) * (j));
 	if (!aux)
 		return (0);
-	if (sb < 0)
+	if (n < 0)
 		aux[0] = '-';
-	ft_aux(n, j, aux, sb);
-	aux[j] = 0;
+	ft_aux(sb, j - 1, aux);
+	aux[j - 1] = 0;
 	return (aux);
 }
 /*
 int	main(void)
 {
-	char *i1 = ft_itoa(147483648);
+	char *i1 = ft_itoa(0);
+	printf("%s", i1);
 	return (0);
 }*/
