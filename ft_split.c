52da21fba68250d0b91564f50d99ab3cc55e1c40
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-10-06 20:57:27 +0200

    actualizacion split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,28 @@
 /*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:50:48 by omartine          #+#    #+#             */
-/*   Updated: 2021/10/05 21:19:43 by omartine         ###   ########.fr       */
+/*   Updated: 2021/10/06 20:56:03 by omartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	wordcount(char const *s, size_t in, size_t i, char c)
+{
+	while (s[in] != 0)
+	{
+		if (s[in] == c)
+			in++;
+		else
+		{
+			i++;
+			while (s[in] != c && s[in] != 0)
+				in++;
+		}
+	}
+	//printf("NUMERO DE PALABRAS--> %zu\n", i);
+	return ((int)i);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -25,28 +42,23 @@ char	**ft_split(char const *s, char c)
 	fin = 0;
 	i = 0;
 	j = 0;
-	if (!s || !c)
+	if (!s)
 		return (0);
-	//contador de palabras
-	while (s[in] != 0)
+	if (c == 0)
 	{
-		if (s[in] == c)
-			in++;
-		else
-		{
-			i++;
-			while (s[in] != c && s[in] != 0)
-				in++;
-		}
+		aux = (char **) malloc (sizeof(char *) * 2);
+		if (!aux)
+			return (0);
+		aux[0] = (char *) malloc(ft_strlen(s) * sizeof(char));
+		ft_strlcpy(aux[0], s, ft_strlen(s) + 1);
+		aux[1] = 0;
+		return (aux);
 	}
-	printf("NUMERO DE PALABRAS--> %zu\n", i);//ok
-
-	//reserva filas--una fila por cada char c encontrado
-	aux = (char **) malloc(sizeof(char) * i);
-
+	i = wordcount(s, in, i, c);
+	aux = (char **) malloc(sizeof(char *) * i);
 	if (!aux)
 		return (0);
-	//guardamos el numero de palabras y reseteamos i
+	aux[i] = 0;
 	z = i;
 	i = 0;
 	in = 0;
@@ -58,33 +70,33 @@ char	**ft_split(char const *s, char c)
 		while (s[fin] != c)
 			fin++;
 		//reserva por caracter separado por char c
-		aux[i] = (char *) malloc(sizeof(char) * (fin - in));
+		aux[i] = (char *) malloc(sizeof(char) * (fin - in + 1));
+		//printf("%zu", fin - in);
 		if (!aux[i])
 			return (0);
-		aux[i][fin - 1] = 0;
+		aux[i][fin - in] = 0;
 		while (in < fin)
 		{
 			aux[i][j] = s[in];
 			in++;
 			j++;
 		}
+		//printf("-%s-\n", aux[i]);
 		i++;
 		j = 0;
-		fin++;//para que no coincida con null
 	}
 	return (aux);
 }
-
-int main(void)
+/*
+int	main(void)
 {
-	char *string = "split  ||this|for|me|||||!|";
-	char **aux;
-	aux = ft_split(string, '|');
+	char **expected = ft_split("\0aa\0bbb", '\0');
 	int i = 0;
-	while (i < 5)
+	while (expected[i] != 0)
+	for (int i = 0; expected[i]; i++)
 	{
-		printf("-%s-\n", aux[i]);
-		i++;
+		if (expected[i] != ((void *)0))
+			printf("CAGASTE");
 	}
 	return 0;
-}
+}*/
