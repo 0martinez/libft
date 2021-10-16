c4e4ec60b135aadb6eb4e5d4a272e93672b9da73
TTR0NC0 <Oscarmr632@gmail.com>
2021-10-16 03:21:41 +0200

    soy un puto genio

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:50:48 by omartine          #+#    #+#             */
-/*   Updated: 2021/10/15 21:07:52 by omartine         ###   ########.fr       */
+/*   Updated: 2021/10/16 03:11:57 by omartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,29 @@ static size_t	wordcount(char const *s, int in, int i, char c)
 	}
 	return (i);
 }
-
-static int	in_size(char const *s, int in, char c)
+//si fallan los tester con ft position 1ºin_pos
+static int	position(char const *s, int pos, char c, int flg)
 {
-	while (s[in] == c && s[in] != 0)
-		in++;
-	return (in);
+	
+	if (flg == 1)
+	{
+		while (s[pos] == c && s[pos] != 0)
+			pos++;
+		return (pos);
+	}
+	while (s[pos] != c && s[pos] != 0)
+		pos++;
+	return (pos);
 }
-
-static int	fin_size(char const *s, int fin, char c)
+/*
+static int	fin_pos(char const *s, int fin, char c)
 {
-	while (s[fin] != c)
+	while (s[fin] != c && s[fin] != 0)
 		fin++;
 	return (fin);
-}
+}*/
 //------------------------------------------------------------
-/*static char	*word(char const *s, int in, int fin)
+static char	*word(char const *s, int in, int fin)
 {
 	int		i;
 	char	*aux;
@@ -58,45 +65,44 @@ static int	fin_size(char const *s, int fin, char c)
 		in++;
 		i++;
 	}
-	//printf("%s", aux);
+	//printf("%s\n", aux);
 	return (aux);
-}*/
+}
 //------------------------------------------------------------
 char	**ft_split(char const *s, char c)
 {
 	int	in;
 	int	fin;
 	int	i;
-	int	j;
+	//int	j;
 	int	adblas;
 	char	**aux;
 
 	in = 0;
 	fin = 0;
 	i = 0;
-	j = 0;
+	//j = 0;
 	if (!s)
 		return (0);
-	i = wordcount(s, in, i, c);
-	aux = (char **) malloc(sizeof(char *) * i + 1);
+	adblas = wordcount(s, in, i, c);
+	aux = (char **) malloc(sizeof(char *) * adblas + 1);
 	if (!aux)
 		return (0);
-	aux[i] = 0;
-	adblas = i;
-	i = 0;
+	aux[adblas] = 0;
 	in = 0;
 	while (i < adblas)
 	{
-		in = in_size(s, in, c);
-		fin = fin_size(s, in, c);
-		aux[i] = (char *) malloc(sizeof(char) * (fin - in + 1));
+		in = position(s, in, c, 1);
+		fin = position(s, in, c, 0);
 		if (!aux[i])
 			return (0);
-		/*aux[i] = word(s, in, fin);
-		if (aux[i] == 0)
+		//aux[i] = (char *) malloc(sizeof(char) * (fin - in + 1));
+		aux[i] = word(s, in, fin);
+		if (!aux[i])
 			return (0);
-		in = fin;*/
-		aux[i] = (char *) malloc(sizeof(char) * (fin - in + 1));
+		in = fin;
+		i++;
+		/*aux[i] = (char *) malloc(sizeof(char) * (fin - in + 1));
 		aux[i][fin - in] = 0;
 		while (in < fin)
 		{
@@ -105,14 +111,21 @@ char	**ft_split(char const *s, char c)
 			j++;
 		}
 		i++;
-		j = 0;
+		j = 0;*/
 	}
 	return (aux);
 }
-/*
+
 int	main(void)
 {
 	char *s = "split  ||this|for|me|||||!|";
-	ft_split(s, '|');
+	char **aux;
+	aux = ft_split(s, '|');
+	int i = 0;
+	while (aux[i] != 0)
+	{
+		printf("%s\n", aux[i]);
+		i++;
+	}
 	return (0);
-}*/
+}
